ad9d90bbf0bbb60656d2366c810fb5866618aab2
Umanda Thathsarani <umathathsarani2003@gmail.com>
2026-06-07 13:11:10 +0530

    Implement Minimax algorithm for AI

---
+++ ai.py
@@ -0,0 +1,31 @@
+import math
+
+def minimax(state, player):
+    max_player = 'O'
+    other_player = 'O' if player == 'X' else 'X'
+
+    if state.current_winner == other_player:
+        return {'position': None, 'score': 1 * (state.num_empty_squares() + 1) if other_player == max_player else -1 * (state.num_empty_squares() + 1)}
+    elif not state.empty_squares():
+        return {'position': None, 'score': 0}
+
+    if player == max_player:
+        best = {'position': None, 'score': -math.inf}
+    else:
+        best = {'position': None, 'score': math.inf}
+
+    for possible_move in state.available_moves():
+        state.make_move(possible_move, player)
+        sim_score = minimax(state, other_player)
+        
+        state.board[possible_move] = ' '
+        state.current_winner = None
+        sim_score['position'] = possible_move
+
+        if player == max_player:
+            if sim_score['score'] > best['score']:
+                best = sim_score
+        else:
+            if sim_score['score'] < best['score']:
+                best = sim_score
+    return best
