6ec1cdebdc0b262ac5746a9dc3a67e66efdc374b
Umanda Thathsarani <umathathsarani2003@gmail.com>
2026-06-07 14:06:57 +0530

    Refactor game logic for dynamic grids, variable win conditions, and dead squares

--- game.py
+++ game.py
@@ -1,15 +1,17 @@
 class TicTacToe:
-    def __init__(self):
-        self.board = [' ' for _ in range(9)]
+    def __init__(self, grid_size=3, win_requirement=3, dead_squares=None):
+        self.grid_size = grid_size
+        self.win_requirement = win_requirement
+        self.dead_squares = dead_squares if dead_squares else []
+        self.board = ['#' if i in self.dead_squares else ' ' for i in range(self.grid_size * self.grid_size)]
         self.current_winner = None
 
     def print_board(self):
-        for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
+        for row in [self.board[i*self.grid_size:(i+1)*self.grid_size] for i in range(self.grid_size)]:
             print('| ' + ' | '.join(row) + ' |')
 
-    @staticmethod
-    def print_board_nums():
-        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
+    def print_board_nums(self):
+        number_board = [[str(i) for i in range(j*self.grid_size, (j+1)*self.grid_size)] for j in range(self.grid_size)]
         for row in number_board:
             print('| ' + ' | '.join(row) + ' |')
 
@@ -31,19 +33,27 @@ class TicTacToe:
         return False
 
     def winner(self, square, letter):
-        row_ind = square // 3
-        row = self.board[row_ind*3 : (row_ind + 1) * 3]
-        if all([spot == letter for spot in row]):
-            return True
-        col_ind = square % 3
-        column = [self.board[col_ind+i*3] for i in range(3)]
-        if all([spot == letter for spot in column]):
-            return True
-        if square % 2 == 0:
-            diagonal1 = [self.board[i] for i in [0, 4, 8]]
-            if all([spot == letter for spot in diagonal1]):
+        r, c = square // self.grid_size, square % self.grid_size
+        directions = [(0, 1), (1, 0), (1, 1), (1, -1)]
+        
+        for dr, dc in directions:
+            count = 1
+            
+            for i in range(1, self.win_requirement):
+                nr, nc = r + dr*i, c + dc*i
+                if 0 <= nr < self.grid_size and 0 <= nc < self.grid_size and self.board[nr * self.grid_size + nc] == letter:
+                    count += 1
+                else:
+                    break
+                    
+            for i in range(1, self.win_requirement):
+                nr, nc = r - dr*i, c - dc*i
+                if 0 <= nr < self.grid_size and 0 <= nc < self.grid_size and self.board[nr * self.grid_size + nc] == letter:
+                    count += 1
+                else:
+                    break
+                    
+            if count >= self.win_requirement:
                 return True
-            diagonal2 = [self.board[i] for i in [2, 4, 6]]
-            if all([spot == letter for spot in diagonal2]):
-                return True
+                
         return False
